f2974b6bb0176bf143df1caedab1f3d105226c67
stefan <rakulle@gmail.com>
2019-10-22 18:17:44 +0200

    Modified gcd.py

--- gcd.py
+++ gcd.py
@@ -3,9 +3,9 @@ def gcd(a, b):
         Functie pentru a calcula
         Cel mai mare divizor comun. 
     '''
-    g = b
     old_g = a
+    g = b   
     while(g  != 0):
         old_g, g = g, old_g % g
     return old_g
-print(gcd(456, 244))
+print(gcd(32, 8))
